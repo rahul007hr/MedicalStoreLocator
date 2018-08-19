478403edcc079dfa7cec1570bf2f7f7fd180b3a0
rahul007hr <rahul007hr@gmail.com>
2018-08-19 07:34:33 -0700

    - code cleaning

--- app/src/androidTest/java/com/medicalstorefinder/medicalstoreslocatorss/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/medicalstorefinder/medicalstoreslocatorss/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.medicalstorefinder.medicalstoreslocator;
+package com.medicalstorefinder.medicalstoreslocatorss;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.medicalstorefinder.medicalstoreslocator", appContext.getPackageName());
+        assertEquals("com.medicalstorefinder.medicalstoreslocatorss", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/medicalstorefinder/medicalstoreslocatorss/ExampleUnitTest.java
+++ app/src/test/java/com/medicalstorefinder/medicalstoreslocatorss/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.medicalstorefinder.medicalstoreslocator;
+package com.medicalstorefinder.medicalstoreslocatorss;
 
 import org.junit.Test;
 
